16ed70ab02ee276e3af0a1a52e20ee73b3d7465a
Connor Gentilcore <connorgentilcore@gmail.com>
2017-04-11 17:03:29 -0400

    complete redesign of script. didn't like how I was handling it in previous versions. this simplifies everything and allows growth

--- 2048.ps1
+++ 2048.ps1
@@ -1,24 +1,68 @@
 
 # definitions
 $debug = 1
+$boardWidth = 30
+$boardHeight = 18
 
-# we will track the value of each object in this array
-# _dev can't access this from within a function. maybe it needs to be global
-$global:objects = @()
-# _dev store the coords. not doing a multi. for now, position 0 will contain coords for object 0, with x and y dilimited with a period (x.y)
-$global:objectCoords = @()
+# track the value in each of the 16 positions (0-15)
+#$pos = @(" ") * 16
+# temporarily use the position number as the value
+$posValue = @(0) * 16
 
-# track all positions of the board at all times. remember that $matrix represents the entire board, not just the play space
-# matrix[y,x]
-$matrix = New-Object 'object[,]' 6,6
+# we'll format posValue and keep indexed here so the value appears in the center of each game piece
+$posValueCentered = @($null) * 16
 
-# define the matrix size by defining each position as a space. we will overwrite each position when necessary
-# need a more elegant way of doing this
-for($i=0;$i -lt 6;$i++) {
-    for($h=0;$h -lt 6;$h++) {
-        $matrix[$h,$i] = " "
-    }
-}
+$posCoords = @($null) * 16
+# created as strings so we can split on the period, then we cast as int later
+$posCoords[0]  =  "2.3"
+$posCoords[1]  =  "9.3"
+$posCoords[2]  = "16.3"
+$posCoords[3]  = "23.3"
+$posCoords[4]  =  "2.7"
+$posCoords[5]  =  "9.7"
+$posCoords[6]  = "16.7"
+$posCoords[7]  = "23.7"
+$posCoords[8]  =  "2.11"
+$posCoords[9]  =  "9.11"
+$posCoords[10] = "16.11"
+$posCoords[11] = "23.11"
+$posCoords[12] =  "2.15"
+$posCoords[13] =  "9.15"
+$posCoords[14] = "16.15"
+$posCoords[15] = "23.15"
+
+
+<#
+    positions: (x,y)
+    each position is the left most character and goes right for a total of 5 characters
+
+    [0]  =   2,3 (through 6,3)
+    [1]  =   9,3
+    [2]  =  16,3
+    [3]  =  23,3
+    [4]  =   2,7
+    [5]  =   9,7
+    [6]  =  16,7
+    [7]  =  23,7
+    [8]  =   2,11
+    [9]  =   9,11
+    [10] =  16,11
+    [11] =  23,11
+    [12] =   2,15
+    [13] =   9,15
+    [14] =  16,15
+    [15] =  23,15
+
+  ###############
+  # 0  1  2  3  #
+  # 4  5  6  7  #
+  # 8  9  10 11 #
+  # 12 13 14 15 #
+  ###############
+
+#>
+
+
 
 
 
@@ -26,169 +70,128 @@ if($debug -eq 1) {
     Set-PSBreakpoint -Variable breakHere
 }
 else {
-    # remove breakpoints, if any exists
     if(Get-PSBreakpoint) {
         Get-PSBreakpoint | Remove-PSBreakpoint
     }
 }
 
-
-# buffer the matrix to the screen
+# buffer the board to the screen
 function Write-Buffer ([string] $str, [int] $x = 0, [int] $y = 0) {
     [console]::setcursorposition($x,$y)
     Write-Host $str -NoNewline
 }
 
-# call this function when we need to spawn a new object. create either a 2 or a 4, then spawn it
+function drawBoard {
+    # let's insert each position
+    for($i=0;$i -lt 16;$i++) {
+
+        # center each posValue
+        if($posValue[$i] -gt 0) {
+            # single digit
+            if($posValue[$i] -lt 10) {
+                $posValueCentered[$i] = "  " + $posValue[$i] + "  "
+            }
+            # two digit (same as single, I just like having each check separate)
+            if(($posValue[$i] -ge 10) -and ($posValue[$i] -le 100)) {
+                $posValueCentered[$i] = "  " + $posValue[$i] + " "
+            }
+            # three digit
+            if(($posValue[$i] -ge 100) -and ($posValue[$i] -le 1000)) {
+                $posValueCentered[$i] = " " + $posValue[$i] + " "
+            }
+            # four digit
+            if(($posValue[$i] -ge 1000) -and ($posValue[$i] -le 10000)) {
+                $posValueCentered[$i] = $posValue[$i] + " "
+            }
+            # five digit
+            if(($posValue[$i] -ge 10000) -and ($posValue[$i] -le 100000)) {
+                $posValueCentered[$i] = $posValue[$i]
+            }
+
+        }
+    
+        $x = [int]$posCoords[$i].Split('.')[0]
+        $y = [int]$posCoords[$i].Split('.')[1]
+
+        write-buffer $posValueCentered[$i] $x $y
+
+        # prepare the symbol to use for drawing the piece border. if a piece is 0 or $null, "erase" the piece border by overwriting the previous border with spaces
+        if(($posValue[$i] -eq 0) -or ($posValue[$i] -eq $null)) {
+            $topBottomSymbol = " "
+            $leftRightSymbol = " "
+        }
+        else {
+            $topBottomSymbol = "_"
+            $leftRightSymbol = "|"
+        }
+
+
+        # draw the piece borders. do the tops/bottoms, then lefts/rights
+        for($b=0;$b -lt 5;$b++) {
+            # top
+            write-buffer $topBottomSymbol ($x+$b) ($y-2)
+
+            # bottom
+            write-buffer $topBottomSymbol ($x+$b) ($y+1)
+
+        }
+        
+        for($b=(-1);$b -lt 2;$b++) {
+            # left
+            write-buffer $leftRightSymbol ($x-1) ($y+$b)
+
+            # right
+            write-buffer $leftRightSymbol ($x+5) ($y+$b)
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
 function createObject {
 
     # determine the starting value of the new object
     #   the probability of a 2 is 90%
     #   the probability of a 4 is 10%
 
-    $rand = Get-Random -min 1 -max 11   # 11 or 10?
+    $r = Get-Random -min 1 -max 11   # 11 or 10?
+    $randValue = $r
 
-    if($rand -eq 4) {
+    if($randValue -eq 4) {
         $value = 4
     }
     else {
         $value = 2
     }
 
-    $global:objects += $value
+    # detect game over. need to add to this a check for if any valid moves exist. if the board is filled and no moves can be made, then game over
+    # count the number of times 0 appears in the array of positions. if it exists 0 times, then the board is filled. game over
+    $isTheBoardFilled = $posValue | group | where name -eq 0 | select -expandproperty count
+    if(!$isTheBoardFilled) {
+        clear
+        write-host "game over"
+        exit
+    }
+    <#
+    #>
 
-
-    # find a random, empty location in the play space for the object
-    #   play space = 1,1 through 4,4
-    #   apparently -max must = 5 to produce 4s, but won't produce 5s?
-    $valid = $false
+    # if we're here, then there is at least one empty position on the board to fill
+    # loop until the random position we've tried is empty
     do {
-        $x = Get-Random -min 1 -max 5
-        $y = Get-Random -min 1 -max 5
+        # randomly choose an empty position
+        $r = Get-Random -min 0 -max 16   # 16 board positions
+        $randPos = $posValue[$r]
+    } until ($randPos -eq 0)
 
-        if($matrix[$y,$x] -eq " ") {
-            # we can spawn the object here
-            $matrix[$y,$x] = $value
-            # save the coords of this object
-            $global:objectCoords += "$x.$y"
-            $valid = $true
-        }
-
-    } until ($valid)
-
-}
-
-function drawBoard {
-
-    for($x=0;$x -lt 6;$x++) {
-        for($y=0;$y -lt 6;$y++) {
-            write-buffer $matrix[$y,$x] $x $y
-        }
-    }
-
-}
-
-# _dev
-function shiftLeft {
-    write-buffer "                " 0 10
-    write-buffer "shiftLeft" 0 10
-}
-
-function shiftDown {
-    write-buffer "                " 0 10
-    write-buffer "shiftDown" 0 10
-}
-
-function shiftUp {
-    write-buffer "                " 0 10
-    write-buffer "shiftUp" 0 10
-}
-
-
-
-
-
-
-
-
-
-# _dev
-# this is working. however, you didn't account for what happens when an object contains >1 digit. 16 is being represented in one square as "1"...
-
-
-# attempt to move objects to the right
-function shiftRight {
-
-    write-buffer "                " 0 10
-    write-buffer "shiftRight" 0 10
-
-    # we want to evaluate each object starting from the right most objects
-    # how can we order $global:objectCoords based on the y coords?
-
-    # evaluate each row, starting from the top
-    for($i=1;$i -le 4;$i++) {
-
-        # evaluate each position in the row, starting from the right
-        for($r=4;$r -ge 1;$r--) {
-
-            # if the position we're checking has an object, let's process that object by moving it right until we cannot anymore
-            if($global:objectCoords -contains "$r.$i") {
-
-                # which object are we working on?
-                $currentObjectIndex = $global:objectCoords.IndexOf("$r.$i")
-
-                do {
-
-                    # get the current object
-                    $currentObject = $global:objects[$currentObjectIndex]
-        
-                    # get current object's x and y
-                    $x = [int]$global:objectCoords[$currentObjectIndex].Split('.')[0]
-                    $y = [int]$global:objectCoords[$currentObjectIndex].Split('.')[1]
-
-                    # free up the old position
-                    $matrix[$y,$x] = " "
-       
-                    # if the adjacent right position is a space
-                    if($matrix[$y,($x+1)] -eq " ") {
-                        # update the coords
-                        $x = $x + 1
-                        $global:objectCoords[$currentObjectIndex] = "$x.$y"
-                    }
-                    # if the adjacent right position is a value that matches the current object
-                    elseif($matrix[$y,($x+1)] -eq $currentObject) {
-                        
-                        # delete the next right object and multiply the current object by 2
-                        #   which object is to the right of me?
-                        $x = $x + 1
-                        $objectToOverwite = $global:objectCoords.IndexOf("$x.$y")
-                        $global:objects[$objectToOverwite] = " "
-                        $global:objectCoords[$objectToOverwite] = $null
-                        $currentObject = $currentObject * 2
-                        
-                        # update the coords
-                        $global:objectCoords[$currentObjectIndex] = "$x.$y"
-                        $global:objects[$currentObjectIndex] = $currentObject
-                    }
-        
-                    # update the matrix
-                    $matrix[$y,$x] = $currentObject
-
-                    # can we move again? we can if the next position to the right is a space or the same number as the current object
-                    if(($matrix[$y,($x+1)] -eq " ") -or ($matrix[$y,($x+1)] -eq $currentObject) ) {
-                        $cannotMove = $false
-                    }
-                    else {
-                        $cannotMove = $true
-                    }
-
-                } until ($cannotMove)
-
-            }
-
-        }
-
-    }
+    $posValue[$r] = $value
+   
 }
 
 
@@ -209,24 +212,18 @@ function shiftRight {
 
 
 
-# make sure we init with one object. on first run of the game, we'll add the second object
-createObject
 
 
-# build the top and bottom borders
-for($i=0;$i -lt 6;$i++) {
-    # top border
-    $matrix[0,$i] = "#"
 
-    # left border
-    $matrix[$i,0] = "#"
 
-    # right border
-    $matrix[$i,5] = "#"
-    
-    # bottom border
-    $matrix[5,$i] = "#"
-}
+
+
+
+
+
+
+
+
 
 
 
@@ -234,57 +231,53 @@ for($i=0;$i -lt 6;$i++) {
 # prepare the board
 clear
 
+# draw the game borders. only needs to happen once, then we just append to the screen with Write-Buffer
+for($i=0;$i -le $boardHeight;$i++) {
+    # top
+    if($i -eq 0) {
+        write-host ("#" * $boardWidth)
+    }
+    # bottom
+    elseif($i -eq ($boardHeight)) {
+        write-host ("#" * $boardWidth)
+    }
+    # middle
+    else {
+        write-host "#" (" " * ($boardWidth-4)) "#"
+    }
+
+}
+
 # play!
-while(1 -eq 1) {
+while (1 -eq 1) {
 
     createObject
 
     drawBoard
-
-    # don't continue until the player inputs a valid key
-    $validInput = $false
-    do {
-        # read the input        
-        $playerInput = [System.Console]::ReadKey() 
-        
-        if(($playerInput.key -eq "UpArrow") -or ($playerInput.key -eq "LeftArrow") -or ($playerInput.key -eq "RightArrow") -or ($playerInput.key -eq "DownArrow") -or ($playerInput.key -eq "Escape")) {
-            $validInput = $true
-        }
-        
-    } until ($validInput)
     
-    # move objects
-    switch ($playerInput.key) {
-        UpArrow {
-            # up
-            shiftUp
-        }
-        LeftArrow {
-            # left
-            shiftLeft
-        }
-        RightArrow {
-            # right
-            shiftRight
-        }
-        DownArrow {
-            # down
-            shiftDown
-        }
-        Escape {
-            exit
-        }
-    }
-
-
-
-
+    write-host ""
+    write-host ""
+    write-host ""
+    write-host ""
+    write-host ""
+    pause   # really want to wait for valid input
 
 }
 
 
 
-
-
-
+# end gracefully
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
 
