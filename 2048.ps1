d6e9166e415df4cf4fe3600bfd60dee0c9f86d0a
Connor Gentilcore <connorgentilcore@gmail.com>
2017-04-12 14:56:00 -0400

    no real major changes, just tons of cleanup

--- 2048.ps1
+++ 2048.ps1
@@ -1,20 +1,15 @@
 
 # definitions
-$debug = 1
-$boardWidth = 30
-$boardHeight = 18
+$debug = 0
 
-# track the value in each of the 16 positions (0-15)
-#$pos = @(" ") * 16
-# temporarily use the position number as the value
+# store the value in each position
 $posValue = @(0) * 16
 
-# we'll format posValue and keep indexed here so the value appears in the center of each game piece
+# used for keeping position values formatted to fit the game piece correctly
 $posValueCentered = @($null) * 16
 
 $posCoords = @($null) * 16
-# created as strings so we can split on the period, then we cast as int later
-$posCoords[0]  =  "2.3"
+$posCoords[0]  =  "2.3"   # created as strings so we can split on the period, then we cast as int later
 $posCoords[1]  =  "9.3"
 $posCoords[2]  = "16.3"
 $posCoords[3]  = "23.3"
@@ -57,6 +52,7 @@ $originalCursorState = [console]::CursorVisible
     [14] =  16,15
     [15] =  23,15
 
+  not to scale, used for reference
   ###############
   # 0  1  2  3  #
   # 4  5  6  7  #
@@ -65,13 +61,6 @@ $originalCursorState = [console]::CursorVisible
   ###############
 
 #>
-
-
-
-
-
-
-
 
 if($debug -eq 1) {
     Set-PSBreakpoint -Variable breakHere
@@ -88,237 +77,25 @@ function Write-Buffer ([string] $str, [int] $x = 0, [int] $y = 0) {
     Write-Host $str -NoNewline
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-function shiftUp {
-
-    for($pos=0;$pos -le 15;$pos=($pos+4)) {
-        if( ($posValue[$pos] -ne 0) -and ($posValue[$pos] -ne $null) -and ($pos -ne 0) -and ($pos -ne 1) -and ($pos -ne 2) -and ($pos -ne 3) ) {
-
-            $posTemp = $pos
-            $done = $false
-            do {
-
-                $nextValueDown = $posValue[$posTemp-4]
-
-                if($nextValueDown -eq 0) {
-                    $posValue[$posTemp-4] = $posValue[$posTemp]
-                    $posValueCentered[$posTemp-4] = $posValueCentered[$posTemp]
-
-                    $posValue[$posTemp] = 0
-                    $posValueCentered[$posTemp] = "     "
-
-                    $posTemp = $posTemp - 4
-                }
-                elseif($nextValueDown -eq $posValue[$posTemp]) {
-                    $posValue[$posTemp-4] = $posValue[$posTemp-4] * 2
-                    $posValueCentered[$posTemp-4] = $posValueCentered[$posTemp]
-
-                    $posValue[$posTemp] = 0
-                    $posValueCentered[$posTemp] = "     "
-
-                    $posTemp = $posTemp - 4
-                }
-                else {
-                    $done = $true
-                }
-
-                if( ($posTemp -eq 0) -or ($posTemp -eq 1) -or ($posTemp -eq 2) -or ($posTemp -eq 3) ) {
-                    $done = $true
-                }
-
-            } until ($done)
-        }
-
-        # if we're at the top row, we'll need to move left and start agian at the bottom
-        if($pos -eq 12) {
-        $pos = (-3)   # we want it to be 4, but we're going to add 4 once we loop in for() again in a moment
-        }
-
-        if($pos -eq 13) {
-            $pos = (-2)
-        }
-
-        if($pos -eq 14) {
-            $pos = (-1)
-        }
-    }
-
-
-
-
-}
-
-
-
-
-
-function shiftDown {
-
-    # check each position (processing from bottom to top)
-    # Up/Down are different from left and right. from bottom to top, we want to process the positions in this order:
-    #   15, 11, 7, 3
-    #   14, 10, 6, 2
-    #   13, 9, 5, 1
-    #   12, 8, 4, 0
-    
-    for($pos=15;$pos -ge 0;$pos=($pos-4)) {
-        if( ($posValue[$pos] -ne 0) -and ($posValue[$pos] -ne $null) -and ($pos -ne 12) -and ($pos -ne 13) -and ($pos -ne 14) -and ($pos -ne 15) ) {
-
-            $posTemp = $pos
-            $done = $false
-            do {
-
-                $nextValueDown = $posValue[$posTemp+4]
-
-                if($nextValueDown -eq 0) {
-                    $posValue[$posTemp+4] = $posValue[$posTemp]
-                    $posValueCentered[$posTemp+4] = $posValueCentered[$posTemp]
-
-                    $posValue[$posTemp] = 0
-                    $posValueCentered[$posTemp] = "     "
-
-                    $posTemp = $posTemp + 4
-                }
-                elseif($nextValueDown -eq $posValue[$posTemp]) {
-                    $posValue[$posTemp+4] = $posValue[$posTemp+4] * 2
-                    $posValueCentered[$posTemp+4] = $posValueCentered[$posTemp]
-
-                    $posValue[$posTemp] = 0
-                    $posValueCentered[$posTemp] = "     "
-
-                    $posTemp = $posTemp + 4
-                }
-                else {
-                    $done = $true
-                }
-
-                if( ($posTemp -eq 12) -or ($posTemp -eq 13) -or ($posTemp -eq 14) -or ($posTemp -eq 15) ) {
-                    $done = $true
-                }
-
-            } until ($done)
-        }
-
-        # if we're at the top row, we'll need to move left and start agian at the bottom
-        if($pos -eq 3) {
-        $pos = 18   # we want it to be 14, but we're going to subtract 4 once we loop in for() again in a moment
-        }
-
-        if($pos -eq 2) {
-            $pos = 17
-        }
-
-        if($pos -eq 1) {
-            $pos = 16
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-function shiftLeft {
-    # check each position (processing from left to right)
-    for($pos=0;$pos -le 15;$pos++) {
-        if( ($posValue[$pos] -ne 0) -and ($posValue[$pos] -ne $null) -and ($pos -ne 0) -and ($pos -ne 4) -and ($pos -ne 8) -and ($pos -ne 12) ) {
-
-            $posTemp = $pos
-            $done = $false
-            do {
-
-                $nextValueLeft = $posValue[$posTemp-1]
-
-                if($nextValueLeft -eq 0) {
-                    $posValue[$posTemp-1] = $posValue[$posTemp]
-                    $posValueCentered[$posTemp-1] = $posValueCentered[$posTemp]
-
-                    $posValue[$posTemp] = 0
-                    $posValueCentered[$posTemp] = "     "
-
-                    $posTemp = $posTemp - 1
-                }
-                elseif($nextValueLeft -eq $posValue[$posTemp]) {
-                    $posValue[$posTemp-1] = $posValue[$posTemp-1] * 2
-                    $posValueCentered[$posTemp-1] = $posValueCentered[$posTemp]
-
-                    $posValue[$posTemp] = 0
-                    $posValueCentered[$posTemp] = "     "
-
-                    $posTemp = $posTemp - 1
-                }
-                else {
-                    $done = $true
-                }
-
-                if( ($posTemp -eq 0) -or ($posTemp -eq 4) -or ($posTemp -eq 8) -or ($posTemp -eq 12) ) {
-                    $done = $true
-                }
-
-            } until ($done)
-        }
-    }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function shiftRight {
     # check each position (processing from right to left)
     for($pos=15;$pos -ge 0;$pos--) {
         # there's nothing to do if the value is empty, or if we're evaluating a value in the right most column (these cant ever move right)
-        if( ($posValue[$pos] -ne 0) -and ($posValue[$pos] -ne $null) -and ($pos -ne 3) -and ($pos -ne 7) -and ($pos -ne 11) -and ($pos -ne 15) ) {
+        if(($posValue[$pos] -ne 0) -and ($posValue[$pos] -ne $null) -and ($pos -ne 3) -and ($pos -ne 7) -and ($pos -ne 11) -and ($pos -ne 15)) {
             # keep working on this position as it moves right until you can no longer do anything with it
             $posTemp = $pos
             $done = $false
             do {
-
                 # check the value to the right
                 $nextValueRight = $posValue[$posTemp+1]
-
                 if($nextValueRight -eq 0) {
                     # we can move to this position
                     # set the right value
                     $posValue[$posTemp+1] = $posValue[$posTemp]
                     $posValueCentered[$posTemp+1] = $posValueCentered[$posTemp]
-
                     # update the old value
                     $posValue[$posTemp] = 0
                     $posValueCentered[$posTemp] = "     "
-
                     # stay with this value
                     $posTemp = $posTemp + 1
                 }
@@ -326,11 +103,9 @@ function shiftRight {
                     # we can multiply the next position by 2
                     $posValue[$posTemp+1] = $posValue[$posTemp+1] * 2
                     $posValueCentered[$posTemp+1] = $posValueCentered[$posTemp]
-
                     # update the old value
                     $posValue[$posTemp] = 0
                     $posValueCentered[$posTemp] = "     "
-
                     # stay with this value
                     $posTemp = $posTemp + 1
                 }
@@ -340,29 +115,140 @@ function shiftRight {
                 }
 
                 # if $posTemp is in the right column, then we're done
-                if( ($posTemp -eq 3) -or ($posTemp -eq 7) -or ($posTemp -eq 11) -or ($posTemp -eq 15) ) {
+                if(($posTemp -eq 3) -or ($posTemp -eq 7) -or ($posTemp -eq 11) -or ($posTemp -eq 15)) {
                     $done = $true
                 }
-
             } until ($done)
         }
     }
 }
 
+function shiftLeft {
+    # check each position (processing from left to right)
+    for($pos=0;$pos -le 15;$pos++) {
+        if(($posValue[$pos] -ne 0) -and ($posValue[$pos] -ne $null) -and ($pos -ne 0) -and ($pos -ne 4) -and ($pos -ne 8) -and ($pos -ne 12)) {
+            $posTemp = $pos
+            $done = $false
+            do {
+                $nextValueLeft = $posValue[$posTemp-1]
+                if($nextValueLeft -eq 0) {
+                    $posValue[$posTemp-1] = $posValue[$posTemp]
+                    $posValueCentered[$posTemp-1] = $posValueCentered[$posTemp]
+                    $posValue[$posTemp] = 0
+                    $posValueCentered[$posTemp] = "     "
+                    $posTemp = $posTemp - 1
+                }
+                elseif($nextValueLeft -eq $posValue[$posTemp]) {
+                    $posValue[$posTemp-1] = $posValue[$posTemp-1] * 2
+                    $posValueCentered[$posTemp-1] = $posValueCentered[$posTemp]
+                    $posValue[$posTemp] = 0
+                    $posValueCentered[$posTemp] = "     "
+                    $posTemp = $posTemp - 1
+                }
+                else {
+                    $done = $true
+                }
 
+                if(($posTemp -eq 0) -or ($posTemp -eq 4) -or ($posTemp -eq 8) -or ($posTemp -eq 12)) {
+                    $done = $true
+                }
+            } until ($done)
+        }
+    }
+}
 
+function shiftUp {
+    # check each position (processing from top to bottom)
+    # shiftUp/Down are different from shiftLeft/Right. we can't use $pos sequencially, need to hop around
+    for($pos=0;$pos -le 15;$pos=($pos+4)) {
+        if(($posValue[$pos] -ne 0) -and ($posValue[$pos] -ne $null) -and ($pos -ne 0) -and ($pos -ne 1) -and ($pos -ne 2) -and ($pos -ne 3)) {
+            $posTemp = $pos
+            $done = $false
+            do {
+                $nextValueDown = $posValue[$posTemp-4]
+                if($nextValueDown -eq 0) {
+                    $posValue[$posTemp-4] = $posValue[$posTemp]
+                    $posValueCentered[$posTemp-4] = $posValueCentered[$posTemp]
+                    $posValue[$posTemp] = 0
+                    $posValueCentered[$posTemp] = "     "
+                    $posTemp = $posTemp - 4
+                }
+                elseif($nextValueDown -eq $posValue[$posTemp]) {
+                    $posValue[$posTemp-4] = $posValue[$posTemp-4] * 2
+                    $posValueCentered[$posTemp-4] = $posValueCentered[$posTemp]
+                    $posValue[$posTemp] = 0
+                    $posValueCentered[$posTemp] = "     "
+                    $posTemp = $posTemp - 4
+                }
+                else {
+                    $done = $true
+                }
 
+                if(($posTemp -eq 0) -or ($posTemp -eq 1) -or ($posTemp -eq 2) -or ($posTemp -eq 3)) {
+                    $done = $true
+                }
+            } until ($done)
+        }
+        # if we're at the top row, we'll need to move left and start agian at the bottom
+        if($pos -eq 12) {
+        $pos = (-3)   # we want it to be 4, but we're going to add 4 once we loop in for() again in a moment
+        }
+        if($pos -eq 13) {
+            $pos = (-2)
+        }
+        if($pos -eq 14) {
+            $pos = (-1)
+        }
+    }
+}
 
+function shiftDown {
+    # check each position (processing from bottom to top)
+    for($pos=15;$pos -ge 0;$pos=($pos-4)) {
+        if(($posValue[$pos] -ne 0) -and ($posValue[$pos] -ne $null) -and ($pos -ne 12) -and ($pos -ne 13) -and ($pos -ne 14) -and ($pos -ne 15)) {
+            $posTemp = $pos
+            $done = $false
+            do {
+                $nextValueDown = $posValue[$posTemp+4]
+                if($nextValueDown -eq 0) {
+                    $posValue[$posTemp+4] = $posValue[$posTemp]
+                    $posValueCentered[$posTemp+4] = $posValueCentered[$posTemp]
+                    $posValue[$posTemp] = 0
+                    $posValueCentered[$posTemp] = "     "
+                    $posTemp = $posTemp + 4
+                }
+                elseif($nextValueDown -eq $posValue[$posTemp]) {
+                    $posValue[$posTemp+4] = $posValue[$posTemp+4] * 2
+                    $posValueCentered[$posTemp+4] = $posValueCentered[$posTemp]
+                    $posValue[$posTemp] = 0
+                    $posValueCentered[$posTemp] = "     "
+                    $posTemp = $posTemp + 4
+                }
+                else {
+                    $done = $true
+                }
 
-
-
-
+                if(($posTemp -eq 12) -or ($posTemp -eq 13) -or ($posTemp -eq 14) -or ($posTemp -eq 15)) {
+                    $done = $true
+                }
+            } until ($done)
+        }
+        # if we're at the top row, we'll need to move left and start agian at the bottom
+        if($pos -eq 3) {
+        $pos = 18   # we want it to be 14, but we're going to subtract 4 once we loop in for() again in a moment
+        }
+        if($pos -eq 2) {
+            $pos = 17
+        }
+        if($pos -eq 1) {
+            $pos = 16
+        }
+    }
+}
 
 function drawBoard {
-
     # let's insert each position
     for($i=0;$i -lt 16;$i++) {
-
         # center each posValue
         if($posValue[$i] -gt 0) {
             # single digit
@@ -385,14 +271,11 @@ function drawBoard {
             if(($posValue[$i] -ge 10000) -and ($posValue[$i] -le 100000)) {
                 $posValueCentered[$i] = $posValue[$i]
             }
-
         }
-    
+
         $x = [int]$posCoords[$i].Split('.')[0]
         $y = [int]$posCoords[$i].Split('.')[1]
-
         write-buffer $posValueCentered[$i] $x $y
-
 
         # prepare the symbol to use for drawing the piece border. if a piece is 0 or $null, "erase" the piece border by overwriting the previous border with spaces. also erase the old value
         if(($posValue[$i] -eq 0) -or ($posValue[$i] -eq $null)) {
@@ -404,12 +287,10 @@ function drawBoard {
             $leftRightSymbol = "|"
         }
 
-
         # draw the piece borders. do the tops/bottoms, then lefts/rights
         for($b=0;$b -lt 5;$b++) {
             # top
             write-buffer $topBottomSymbol ($x+$b) ($y-2)
-
             # bottom
             write-buffer $topBottomSymbol ($x+$b) ($y+1)
 
@@ -418,30 +299,16 @@ function drawBoard {
         for($b=(-1);$b -lt 2;$b++) {
             # left
             write-buffer $leftRightSymbol ($x-1) ($y+$b)
-
             # right
             write-buffer $leftRightSymbol ($x+5) ($y+$b)
-
         }
 
-        # _dev
         # set the cursor position off the board. I was leaving the cursor in the last position and overwriting stuff. a cursor pos reset used to exist in write-buffer, but putting it here will reduce overhead, not repeating this instruction on every call of write-buffer
-        [console]::setcursorposition(0,30)
-
+        [console]::setcursorposition(0,40)
     }
 }
 
-
-
-
-
-
-
-
-
-
 function createObject {
-
     # determine the starting value of the new object
     #   the probability of a 2 is 90%
     #   the probability of a 4 is 10%
@@ -479,34 +346,7 @@ function createObject {
     } until ($randPos -eq 0)
 
     $posValue[$r] = $value
-   
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -520,58 +360,55 @@ function createObject {
 clear
 
 # draw the game borders. only needs to happen once, then we just append to the screen with Write-Buffer
-for($i=0;$i -le $boardHeight;$i++) {
+for($i=0;$i -le 18;$i++) {
     # top
     if($i -eq 0) {
-        write-host ("#" * $boardWidth)
+        write-host ("#" * 30)
     }
     # bottom
-    elseif($i -eq ($boardHeight)) {
-        write-host ("#" * $boardWidth)
+    elseif($i -eq (18)) {
+        write-host ("#" * 30)
     }
     # middle
     else {
-        write-host "#" (" " * ($boardWidth-4)) "#"
+        write-host "#" (" " * (30-4)) "#"
     }
 
 }
+
+
+
+
+
 
 
 # play!
 while (1 -eq 1) {
 
     createObject
-
     drawBoard
     
     # don't continue until the player inputs a valid key
     $validInput = $false
     do {
-        # read the input        
         $playerInput = [System.Console]::ReadKey() 
-        
         if(($playerInput.key -eq "UpArrow") -or ($playerInput.key -eq "LeftArrow") -or ($playerInput.key -eq "RightArrow") -or ($playerInput.key -eq "DownArrow") -or ($playerInput.key -eq "Escape")) {
             $validInput = $true
         }
-        
     } until ($validInput)
     
-    # move objects
+    # move stuff
     switch ($playerInput.key) {
         UpArrow {
-            # up
             shiftUp
         }
         LeftArrow {
-            # left
             shiftLeft
         }
         RightArrow {
-            # right
             shiftRight
         }
         DownArrow {
-            # down
             shiftDown
         }
         Escape {
@@ -579,28 +416,6 @@ while (1 -eq 1) {
             exit
         }
     }
-
 }
 
-
-
-
-
-
-
-
-# end gracefully
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
 
